3a27539e802b98f35f326dc9f38957530f8cf282
XuChengL <262355580@qq.com>
2015-07-21 15:53:05 +0800

    heeloo
    hello

---
+++ zl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zl
+//
+//  Created by daxucheng on 15/7/21.
+//  Copyright (c) 2015年 xucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zl
+//
+//  Created by daxucheng on 15/7/21.
+//  Copyright (c) 2015年 xucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
